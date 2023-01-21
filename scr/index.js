19e93df71f780a1917361a9f8adc8d851da17fa1
Volha Iuzhychenka <olga.ivzhich@gmail.com>
2023-01-21 20:16:32 +0100

    index.js added

---
+++ scr/index.js
@@ -0,0 +1,36 @@
+const DATABASE = {
+    dealer: {
+        title: 'Trucks & Buses Vilnius LTD'
+    },
+    trucks: [
+        {
+            vin: 1112,
+            color: 'Red',
+            carryWeight: 10
+        },
+        {
+            vin: 2332,
+            color: 'Yellow',
+            carryWeight: 20
+        },
+        {
+            vin: 5234,
+            color: 'Green',
+            carryWeight: 70
+        }
+    ],
+    buses: [
+        {
+            vin: 1112,
+            color: 'Green',
+            maxPassengers: 50
+        },
+        {
+            vin: 6543,
+            color: 'Yellow',
+            maxPassengers: 25
+        }
+    ]
+}
+
+console.log(DATABASE)
